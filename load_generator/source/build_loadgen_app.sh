bcc74d399652e6051ced587dfb01d96edb0a15c2
Frank Kootte <fkootte@bol.com>
2023-04-11 13:53:47 +0200

    - add pushing Docker image to registry

--- load_generator/source/build_loadgen_app.sh
+++ load_generator/source/build_loadgen_app.sh
@@ -1,3 +1,8 @@
 ./mvnw package -DskipTests
-docker build . --build-arg PARENT_IMAGE=eu.gcr.io/bolcom-stg-jvm-f30/debian-zulu-jdk-17:latest -t etude-loadgen
+
+docker build . \
+  --build-arg PARENT_IMAGE="eu.gcr.io/bolcom-stg-jvm-f30/debian-zulu-jdk-17:latest" \
+  -t "eu.gcr.io/bolcom-pro-reco-analytics-fcc/etude-loadgen"
+
+docker push "eu.gcr.io/bolcom-pro-reco-analytics-fcc/etude-loadgen"
 
