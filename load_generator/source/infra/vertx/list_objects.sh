8a842d726ac4422e33a0bfd251eb6f9482453091
Frank Kootte <fkootte@bol.com>
2023-04-12 09:48:55 +0200

    cleanup

--- load_generator/source/infra/vertx/list_objects.sh
+++ load_generator/source/infra/vertx/list_objects.sh
@@ -10,7 +10,6 @@ export OBJECT_TYPE="${1}"
 kubectl --context bolcom-pro-default --namespace reco-analytics delete job "etude-list-objects" --ignore-not-found=true --timeout=10m
 
 envsubst < ./list_objects_job.yaml > "/tmp/list_objects_job.yaml"
-
-#cat "/tmp/list_objects_job.yaml"
+# cat "/tmp/list_objects_job.yaml"
 
 kubectl --context bolcom-pro-default --namespace reco-analytics apply --namespace reco-analytics -f - < "/tmp/list_objects_job.yaml"
