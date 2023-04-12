c4fb8f3b999ceb9f2a14a32a004505fc1433ff83
Frank Kootte <fkootte@bol.com>
2023-04-12 09:17:25 +0200

    prefix k8s resource names

--- load_generator/source/infra/loadgen/deploy_loadgen.sh
+++ load_generator/source/infra/loadgen/deploy_loadgen.sh
@@ -6,12 +6,12 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-export LOADTEST_NAME=:"${1}"
+export LOADTEST_NAME="${1}"
 
-kubectl --context bolcom-pro-default --namespace reco-analytics delete job "${LOADTEST_NAME}" --ignore-not-found=true --timeout=10m
+kubectl --context bolcom-pro-default --namespace reco-analytics delete job "etude-${LOADTEST_NAME}" --ignore-not-found=true --timeout=10m
 
 envsubst < ./deploy_loadgen_job.yaml > "/tmp/deploy_loadgen_job.${LOADTEST_NAME}.yaml"
-# cat "/tmp/deploy_loadgen_job.${LOADTEST_NAME}.yaml"
+ cat "/tmp/deploy_loadgen_job.${LOADTEST_NAME}.yaml"
 
 kubectl --context bolcom-pro-default --namespace reco-analytics apply --namespace reco-analytics -f - < "/tmp/deploy_loadgen_job.${LOADTEST_NAME}.yaml"
 
