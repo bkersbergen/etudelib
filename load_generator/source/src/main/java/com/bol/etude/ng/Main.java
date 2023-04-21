130f501f066c3716908aaa8e9f21a0157209194e
Frank Kootte <fkootte@bol.com>
2023-04-21 16:35:42 +0200

    - adapting max rps & duration to observed capacity of vertex deployments of noop_eager models

--- load_generator/source/src/main/java/com/bol/etude/ng/Main.java
+++ load_generator/source/src/main/java/com/bol/etude/ng/Main.java
@@ -88,7 +88,7 @@ public class Main {
         try (persister; requester) {
             System.out.println("Scenario.run()");
 
-            rampWithBackPressure(1000, ofSeconds(600), (request) -> {
+            rampWithBackPressure(500, ofSeconds(600), (request) -> {
                 executor.execute(() -> {
                     request.fly();
 
