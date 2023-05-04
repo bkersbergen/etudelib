2c59c2e3290250e6c5cf4216a2710bfcc347dfbd
bkersbergen <bkersbergen@bol.com>
2023-05-04 10:20:17 +0200

    - Many fixes

---
+++ load_generator/source/predict_localhost.sh
@@ -0,0 +1,22 @@
+PROJECT_ID="bolcom-pro-reco-analytics-fcc"
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "instances": [
+    {
+  "context": [
+   1, 2, 3
+  ]
+}
+  ],
+  "parameters": [
+    {"runtime":  "$RUNTIME"}
+  ]
+}
+
+EOF
+}
+
+curl -H "Content-Type: application/json" -X POST http://localhost:7080/predictions/model/1.0/ -d "$(generate_post_data)"
