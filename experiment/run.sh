bd3cba4ab5ffb223a585b43acadf9b57ca376616
bkersbergen <bkersbergen@bol.com>
2023-05-30 14:49:38 +0200

    - Run load generator in cloud

--- experiment/run.sh
+++ experiment/run.sh
@@ -4,8 +4,8 @@ set -e
 DIR="$(dirname "$0")"
 
 DEPLOY=false
-TEST=false
-DESTROY=true
+TEST=true
+DESTROY=false
 
 MACHINES=('n1-highmem-4')
 ACCELERATIONS=(false) # (false 'NVIDIA_TESLA_T4')
@@ -17,12 +17,15 @@ function normalize() {
 
 for machine in "${MACHINES[@]}"; do
   for acceleration in "${ACCELERATIONS[@]}"; do
-    model=resnet-18-custom-handler
+    size=1000000
+    TARGET_RPS=100
+    RAMP_DURATION_MINUTES=10
+    model=noop_bolcom_c${size}_t50_jitopt
     hardware="$(normalize "${machine}")$(if [ "${acceleration}" != "false" ]; then echo "_$(normalize "${acceleration}")"; fi)"
 
     if [ "${DEPLOY}" = "true" ]; then
       "${DIR}"/vertex/create_endpoint.sh "${model}_${hardware}"
-      "${DIR}"/vertex/deploy_model.sh "europe-docker.pkg.dev/vertex-ai/prediction/pytorch-cpu.1-13:latest" "gs://bolcom-pro-reco-analytics-fcc-shared/barrie_etude/trained/resnet-18-custom-handler/"
+      "${DIR}"/vertex/deploy_model.sh "europe-docker.pkg.dev/vertex-ai/prediction/pytorch-cpu.1-13:latest" "gs://bolcom-pro-reco-analytics-fcc-shared/barrie_etude/trained/${model}/"
 
       if [ "${acceleration}" != "false" ]; then
         "${DIR}"/vertex/deploy_endpoint_model.sh "${model}_${hardware}" "${model}" "${machine}" "${acceleration}" '1'
@@ -30,6 +33,14 @@ for machine in "${MACHINES[@]}"; do
         "${DIR}"/vertex/deploy_endpoint_model.sh "${model}_${hardware}" "${model}" "${machine}"
       fi
     fi
+
+    if [ "${TEST}" = "true" ]; then
+      ENDPOINT_PATH=$("$DIR"/vertex/gcloud/endpoints_state.sh | jq -r ".[] | select(.display == \"${model}_${hardware}\") | select(.models[].display == \"${model}\") | .name")
+      ENDPOINT_URI="https://europe-west4-aiplatform.googleapis.com/v1/${ENDPOINT_PATH}:predict"
+      REPORT_URI="gs://bolcom-pro-reco-analytics-fcc-shared/etude_reports/${model}_${hardware}.avro"
+      "${DIR}"/loadgen/deploy_loadgen.sh "${ENDPOINT_URI}" "${size}" "${REPORT_URI}" "${TARGET_RPS}" "${RAMP_DURATION_MINUTES}"
+    fi
+
 
     if [ "${DESTROY}" = "true" ]; then
       "${DIR}"/vertex/purge_endpoint.sh "${model}_${hardware}"
