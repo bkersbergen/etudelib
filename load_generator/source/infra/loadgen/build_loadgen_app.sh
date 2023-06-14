aa7ba4f0cdc82d3eda2905717a8c9bb8bce2b1de
bkersbergen <bkersbergen@bol.com>
2023-06-14 11:33:42 +0200

    - Update scripts to match new Kubernetes cluster

--- load_generator/source/infra/loadgen/build_loadgen_app.sh
+++ load_generator/source/infra/loadgen/build_loadgen_app.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+PROJECT_ID=bk47475
 ./mvnw clean package -DskipTests
 
 docker build . \
   --build-arg PARENT_IMAGE="eu.gcr.io/bolcom-stg-jvm-f30/debian-zulu-jdk-17:latest" \
   --platform linux/amd64 \
-  --tag "eu.gcr.io/bolcom-pro-reco-analytics-fcc/etude-loadgen:latest"
+  --tag "eu.gcr.io/${PROJECT_ID}/etude-loadgen:latest"
 
-docker push "eu.gcr.io/bolcom-pro-reco-analytics-fcc/etude-loadgen:latest"
+docker push "eu.gcr.io/${PROJECT_ID}/etude-loadgen:latest"
--- load_generator/source/infra/loadgen/deploy_loadgen.sh
+++ load_generator/source/infra/loadgen/deploy_loadgen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+PROJECT_ID=bk47475
 if [ $# -lt 5 ]; then
     echo "requires args 'VERTEX_ENDPOINT', 'CATALOG_SIZE', 'REPORT_LOCATION', 'TARGET_RPS', 'RAMP_DURATION_MINUTES'"
     exit 1
@@ -19,17 +20,17 @@ echo "loadtest.run(endpoint = '${VERTEX_ENDPOINT}', catalog_size = '${CATALOG_SI
 HASH=$(sum <<< "${VERTEX_ENDPOINT}" | cut -f 1 -d ' ')
 JOB_NAME="etude-run-loadtest-${HASH}"
 
-kubectl --context bolcom-pro-default --namespace reco-analytics delete job "${JOB_NAME}" --grace-period=0 --wait=true --ignore-not-found=true --timeout=5m
+kubectl delete job "${JOB_NAME}" --grace-period=0 --wait=true --ignore-not-found=true --timeout=5m
 
-export JOB_NAME VERTEX_ENDPOINT CATALOG_SIZE REPORT_LOCATION TARGET_RPS RAMP_DURATION_MINUTES
+export JOB_NAME VERTEX_ENDPOINT CATALOG_SIZE REPORT_LOCATION TARGET_RPS RAMP_DURATION_MINUTES PROJECT_ID
 envsubst < "$DIR"/deploy_loadgen_job.yaml > "/tmp/deploy_loadgen_job.${HASH}.yaml"
-export -n JOB_NAME VERTEX_ENDPOINT CATALOG_SIZE REPORT_LOCATION TARGET_RPS RAMP_DURATION_MINUTES
+export -n JOB_NAME VERTEX_ENDPOINT CATALOG_SIZE REPORT_LOCATION TARGET_RPS RAMP_DURATION_MINUTES PROJECT_ID
 
-kubectl --context bolcom-pro-default --namespace reco-analytics apply --namespace reco-analytics -f - < "/tmp/deploy_loadgen_job.${HASH}.yaml"
-POD_NAME=$(kubectl get pods --context bolcom-pro-default --namespace reco-analytics -l job-name="$JOB_NAME" -o custom-columns=:metadata.name | tr -d '\n')
-POD_READY=$(kubectl --context bolcom-pro-default --namespace reco-analytics wait --for=condition=Ready pod/"$POD_NAME" --timeout=30m)
+kubectl apply -f - < "/tmp/deploy_loadgen_job.${HASH}.yaml"
+POD_NAME=$(kubectl get pods -l job-name="$JOB_NAME" -o custom-columns=:metadata.name | tr -d '\n')
+POD_READY=$(kubectl wait --for=condition=Ready pod/"$POD_NAME" --timeout=30m)
 
-LOGS=$(kubectl --context bolcom-pro-default --namespace reco-analytics logs pod/"${POD_NAME}" --follow)
+LOGS=$(kubectl logs pod/"${POD_NAME}" --follow)
 if [[ "$LOGS" =~ .*"Test.ok()".* ]]; then
   echo "loadtest.ok()"
   exit 1
