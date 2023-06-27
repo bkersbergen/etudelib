a48d9904f64415cc4e85f8806104bb0fe9a62049
bkersbergen <bkersbergen@bol.com>
2023-06-27 13:10:29 +0200

    - New week, greenfield start. Fix old project name

--- load_generator/source/infra/loadgen_playground.sh
+++ load_generator/source/infra/loadgen_playground.sh
@@ -1,4 +1,4 @@
 DIR="$(dirname "$0")"
 PROJECT_ID=bk47477
 
-"${DIR}"/loadgen/deploy_loadgen.sh http://10.58.128.110:8080/predictions/model/1.0 1000000 gs://bk47476-shared/results/static.avro 1000 10
+"${DIR}"/loadgen/deploy_loadgen.sh http://10.58.128.110:8080/predictions/model/1.0 1000000 gs://${PROJECT_ID}-shared/results/static.avro 1000 10
