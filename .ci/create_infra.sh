b87a2d90a48a4b08eeb25e4e1a0bc207c89baa8a
bkersbergen <bkersbergen@bol.com>
2023-07-10 10:15:28 +0200

    - Automatically create all cloud infrastructure and setup permissions

---
+++ .ci/create_infra.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PROJECT_ID=bk47479
+`
+gcloud config set project ${PROJECT_ID}
+`
+gcloud services enable container.googleapis.com
+
+gcloud container --project "${PROJECT_ID}" clusters create-auto "autopilot-cluster-1" --region "europe-west4" --release-channel "regular" --network "projects/${PROJECT_ID}/global/networks/default" --subnetwork "projects/${PROJECT_ID}/regions/europe-west4/subnetworks/default" --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22"
+``
+gcloud container clusters get-credentials autopilot-cluster-1 \
+    --region europe-west4 \
+    --project="${PROJECT_ID}"
+``
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: etudelib
+EOF
+
+`
+gcloud iam service-accounts create etudelib \
+    --description="etudelib" \
+    --display-name="etudelib"
+
+gcloud storage buckets create gs://${PROJECT_ID}-shared
+`
+gcloud storage buckets add-iam-policy-binding gs://${PROJECT_ID}-shared \
+    --member "serviceAccount:etudelib@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/storage.insightsCollectorService"
+gcloud storage buckets add-iam-policy-binding gs://${PROJECT_ID}-shared \
+    --member "serviceAccount:etudelib@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/storage.objectAdmin"
+`
+
+`
+gcloud iam service-accounts add-iam-policy-binding etudelib@${PROJECT_ID}.iam.gserviceaccount.com \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:${PROJECT_ID}.svc.id.goog[default/etudelib]"
+
+kubectl annotate serviceaccount etudelib \
+    --namespace default \
+    iam.gke.io/gcp-service-account=etudelib@${PROJECT_ID}.iam.gserviceaccount.com
+
+kubectl create rolebinding etudelib-rolebinding \
+   --clusterrole=view \
+   --serviceaccount=default:etudelib \
+   --namespace=default
+
+
