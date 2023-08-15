30a2660434565f8fef9ee90e9873d65dc22a7dd5
bkersbergen <bkersbergen@bol.com>
2023-08-15 08:41:47 +0200

    - Centralize the Docker images

--- infra/loadgen_run.sh
+++ infra/loadgen_run.sh
@@ -2,6 +2,14 @@
 # hard failure if any required env var is not set
 set -o nounset
 
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of parameters. PROJECT_ID is missing"
+    echo "Usage: $0 bk47471"
+    exit 2
+fi
+
+PROJECT_ID=$1
+
 DIR="$(dirname "$0")"
 echo Determining service endpoint for etudelibrust
 endpoint_ip=$(kubectl get service etudelibrust -o yaml | awk '/clusterIP:/ { gsub("\"","",$2); print $2 }')
