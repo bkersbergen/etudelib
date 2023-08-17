ff93cdb50e7aa7e49c0da0090b42a4155806794d
bkersbergen <bkersbergen@bol.com>
2023-08-17 09:50:35 +0200

    - Create script to deploy rust service with model path in a bucket as argument

--- .k8s/experiment_pipeline.sh
+++ .k8s/experiment_pipeline.sh
@@ -11,11 +11,17 @@ PROJECT_ID="${1}"
 
 echo "$0.run(PROJECT_ID = '${PROJECT_ID}')"
 
+models=('core' 'gru4rec')
 devices=('cpu' 'gpu')
 runtimes=('jit' 'onnx')
+c_values=(10000 1000000 10000000)
 
 for device in "${devices[@]}"; do
    for runtime in "${runtimes[@]}"; do
-      echo "$device $runtime"
+     for model in "${models[@]}"; do
+       for c in "${c_values[@]}"; do
+         echo "$model $device $runtime $c"
+       done
+     done
    done
 done
