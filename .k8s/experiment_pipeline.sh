439058545a84fc5a850e5e5f9d30ab68cfb6d3ff
bkersbergen <bkersbergen@bol.com>
2023-08-17 11:33:34 +0200

    - Create script to deploy rust service with model path in a bucket as argument

--- .k8s/experiment_pipeline.sh
+++ .k8s/experiment_pipeline.sh
@@ -10,17 +10,39 @@ DIR="$(dirname "$0")"
 PROJECT_ID="${1}"
 
 echo "$0.run(PROJECT_ID = '${PROJECT_ID}')"
+PROJECT_ID="${1}"
+MODEL_PATH="${2}"
+PAYLOAD_PATH="${3}"
+
+
+# Function to check if a file exists
+file_exists() {
+  if [[ $1 == gs://* ]]; then
+    gsutil -q stat "$1"
+  else
+    [ -f "$1" ]
+  fi
+}
 
 models=('core' 'gru4rec')
 devices=('cpu' 'gpu')
-runtimes=('jit' 'onnx')
-c_values=(10000 1000000 10000000)
+runtimes=('jitopt' 'onnx')
+c_values=(10000 1000000)
 
 for device in "${devices[@]}"; do
    for runtime in "${runtimes[@]}"; do
      for model in "${models[@]}"; do
        for c in "${c_values[@]}"; do
-         echo "$model $device $runtime $c"
+         MODEL_PATH="gs://${PROJECT_ID}-shared/model_store/${model}_bolcom_c${c}_t50_${device}/${model}_bolcom_c${c}_t50_${device}_${runtime}.pth"
+         PAYLOAD_PATH="gs://${PROJECT_ID}-shared/model_store/${model}_bolcom_c${c}_t50_${device}/${model}_bolcom_c${c}_t50_${device}_payload.yaml"
+         # Check if both files exist
+         if ! (file_exists "$MODEL_PATH" && file_exists "$PAYLOAD_PATH"); then
+           echo "Error: One or both files do not exist."
+           echo "$MODEL_PATH"
+           echo "$PAYLOAD_PATH"
+           exit 1
+         fi
+         echo "deploy model $MODEL_PATH $PAYLOAD_PATH"
        done
      done
    done
