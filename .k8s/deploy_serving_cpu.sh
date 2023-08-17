2f4a3bcd0fb4c51256f71dad80e971cff19af31a
bkersbergen <bkersbergen@bol.com>
2023-08-17 09:10:24 +0200

    - Create script to deploy rust service with model path in a bucket as argument

---
+++ .k8s/deploy_serving_cpu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 PROJECT_ID MODEL_PATH PAYLOAD_PATH"
+    exit 1
+fi
+
+DIR="$(dirname "$0")"
+PROJECT_ID="${1}"
+MODEL_PATH="${2}"
+PAYLOAD_PATH="${3}"
+YAML_TEMPLATE=${DIR}/etudelibrust-deployment_cpu.yaml
+
+# Check if the YAML file exists
+if [ ! -f "$YAML_TEMPLATE" ]; then
+    echo "Error: YAML file $YAML_TEMPLATE not found."
+    exit 1
+fi
+
+kubectl delete deployment etudelibrust
+
+kubectl apply -f <(
+  sed -e "s|\${PROJECT_ID}|${PROJECT_ID}|" \
+    -e "s|\${MODEL_PATH}|${MODEL_PATH}|" \
+    -e "s|\${PAYLOAD_PATH}|${PAYLOAD_PATH}|" \
+    $YAML_TEMPLATE
+    )
+
+kubectl apply -f ${DIR}/etudelibrust-service.yaml
