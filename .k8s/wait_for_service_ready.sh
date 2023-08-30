ab217653bced06247fcae56f1b9243f9fdfd7a92
bkersbergen <bkersbergen@bol.com>
2023-08-30 20:33:46 +0200

    - Fix intermittent kill signal near the end of loadtests. Preventing data copy to storage bucket

--- .k8s/wait_for_service_ready.sh
+++ .k8s/wait_for_service_ready.sh
@@ -9,7 +9,7 @@ SERVING_NAME="${1}"
 
 # Function to check if the pod is ready
 check_pod_ready() {
-    POD_NAME=$(kubectl get pods | grep -v Terminating | grep ${SERVING_NAME} | awk '{print $1}')
+    POD_NAME=$(kubectl get pods | grep -v Terminating | grep ${SERVING_NAME} | awk '{print $1}' | head -n 1 )
     local pod_status=$(kubectl get pod ${POD_NAME} -o jsonpath='{.status.containerStatuses[0].ready}')
     [ "$pod_status" == "true" ]
 }
