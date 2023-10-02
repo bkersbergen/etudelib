fe87d961a64b4e1b023960bb39941b7b3065a066
bkersbergen <bkersbergen@bol.com>
2023-10-02 11:00:32 +0200

    - Using Journeys in a single thread

---
+++ load_generator/source/src/test/java/com/bol/etude/ng/SyntheticJourneySupplierTest.java
@@ -0,0 +1,32 @@
+package com.bol.etude.ng;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SyntheticJourneySupplierTest {
+
+    @Test
+    public void shouldDetermineSingleThreaded() {
+        int C = 1000000;
+        SyntheticJourneySupplier journeys = new SyntheticJourneySupplier(C);
+        journeys.fit(5.597568416279968, 8.0E-5, 3.650557039874508);
+        Journeys underTest = new Journeys(journeys);
+        Instant start = Instant.now();
+        int clicks = 0;
+        for ( int i = 0 ; i < 10000; i++) {
+            Journeys.Journey journey = underTest.pull();
+            for (int t = 0 ; t < journey.size(); t++) {
+                List<Long> sessionItems = journey.item();
+                clicks += 1;
+            }
+        }
+        Duration duration = Duration.between(start, Instant.now());
+        System.out.println(clicks / duration.toSeconds());
+    }
+
+}
